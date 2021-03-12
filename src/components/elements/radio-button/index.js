582683994e43dedc72106694a80932943ec25a24
AlexPotanes <potanes@yandex.ru>
2021-03-12 14:32:30 +0300

    Очистка от лишних компонентов, отладка радиокнопки, правки readme

--- src/components/elements/radio-button/index.js
+++ src/components/elements/radio-button/index.js
@@ -7,73 +7,39 @@ import './style.less';
 class RadioButton extends Component {
   static propTypes = {
     value: PropTypes.node.isRequired,
-    type: PropTypes.string,
-    placeholder: PropTypes.string,
     name: PropTypes.string.isRequired,
     title: PropTypes.string,
-    required: PropTypes.bool,
-    focused: PropTypes.bool,
     disabled: PropTypes.bool,
-    tabIndex: PropTypes.number,
-    autocomplete: PropTypes.bool,
     onChange: PropTypes.func.isRequired,
-    onFocus: PropTypes.func,
-    onBlur: PropTypes.func,
     theme: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
     checked: PropTypes.bool,
   };
 
   static defaultProps = {
-    onBlur: () => {},
     onChange: () => {},
-    onFocus: () => {},
     disabled: false,
-    type: 'text',
+    checked: false,
   };
 
   onChange = e => {
     const { onChange } = this.props;
-
     const value = e.target.value;
     return onChange(value);
   };
 
-  onFocus = e => this.props.onFocus(e);
-
-  onBlur = () => this.props.onBlur();
-
   render() {
-    const {
-      type,
-      placeholder,
-      required,
-      focused,
-      value,
-      disabled,
-      tabIndex,
-      autocomplete,
-      name,
-      title,
-      checked,
-    } = this.props;
+    const { value, disabled, name, title, checked } = this.props;
 
     return (
       <label className={themes('RadioButton', this.props.theme)}>
         <input
           className="RadioButton__input"
           value={value}
-          type={type}
+          type="radio"
           name={name}
-          placeholder={placeholder}
-          tabIndex={tabIndex}
           disabled={disabled}
           checked={checked}
-          required={required}
-          autoFocus={focused}
-          autoComplete={autocomplete ? 'on' : 'off'}
           onChange={this.onChange}
-          onFocus={this.onFocus}
-          onBlur={this.onBlur}
         />
         <span className="RadioButton__label">{title}</span>
       </label>
